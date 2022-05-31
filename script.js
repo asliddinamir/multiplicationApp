8b194ecefdef210edf402b0828a4d8c2373f891f
Asliddin Amirov <asliddinamir2@gmail.com>
2022-05-31 15:51:30 +0300

    Multiplication App For Kids

---
+++ script.js
@@ -0,0 +1,36 @@
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
+const questionEl = document.getElementById('question');
+const inputEl = document.getElementById('input')
+const formEl = document.getElementById('form');
+const scoreEL = document.getElementById('score')
+
+let score = JSON.parse(localStorage.getItem('score'));
+
+if (!score) {
+    score = 0;
+}
+
+scoreEL.innerText = `Score: ${score}`;
+
+questionEl.innerText = `What is ${num1} multiply by ${num2}`;
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener('submit', () => {
+    const userAns = +inputEl.value
+    if (userAns === correctAns) {
+        score++;
+        updateLocalStorage();
+    } else {
+        score--;
+        updateLocalStorage();
+    }
+});
+
+function updateLocalStorage() {
+    localStorage.setItem("score", JSON.stringify(score))
+};
+
+
